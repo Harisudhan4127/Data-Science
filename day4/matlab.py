a4fff7354ede788108c7cbec22fe58a0020dcf1f
Harisudhan4127 <harisudhan4127@gmail.com>
2025-03-12 23:03:12 +0530

    day 4

--- day4/matlab.py
+++ day4/matlab.py
@@ -1,10 +1,19 @@
+# import matplotlib.pyplot as plt
+# import pandas as pd
+# df = pd.read_csv(r"Data/salary.csv")
+# salary = df['salary'].fillna(df['salary'].mean())
+# dept = df['dept']
+# plt.xlabel("Dept")
+# plt.ylabel("Salary")
+# plt.bar(dept, salary)
+# plt.show()
+# print(salary)
+
 import matplotlib.pyplot as plt
 import pandas as pd
-df = pd.read_csv(r"Data/salary.csv")
-salary = df['salary'].fillna(df['salary'].mean())
-dept = df['dept']
-plt.xlabel("Dept")
-plt.ylabel("Salary")
-plt.bar(dept, salary)
+df = pd.read_csv(r'Data/sales.csv')
+plt.title('Sales-Data')
+plt.plot(df['year'], df['sales'])
+plt.xlabel('year')
+plt.ylabel('sales')
 plt.show()
-print(salary)
