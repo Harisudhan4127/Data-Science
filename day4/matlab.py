d5ec03401e33727cdb4035112bbf40002fdaf4ef
Harisudhan4127 <harisudhan4127@gmail.com>
2025-03-12 22:31:08 +0530

    day 4

--- day4/matlab.py
+++ day4/matlab.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-df = pd.read_csv(r"Data-Science/Data/salary.csv")
+df = pd.read_csv(r"Data/salary.csv")
 salary = df['salary'].fillna(df['salary'].mean())
 dept = df['dept']
 plt.xlabel("Dept")
