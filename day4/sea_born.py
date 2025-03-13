95e69477d9101670c95391b56a404aba2d76b2b0
Harisudhan4127 <harisudhan4127@gmail.com>
2025-03-13 06:00:20 +0530

    day 4

--- day4/sea_born.py
+++ day4/sea_born.py
@@ -1,11 +1,19 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
-import numpy as np
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# import numpy as np
+
 # da = np.array([1,3,6,9,12,15,18,21,24,27,30,100])
 # sns.boxplot(da)
 # plt.show()
 
 # Heatmap, pairplot, countplot, swarmplot and stripplot
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
 iris = sns.load_dataset('iris')
-print(np.random.randint(1,50))
+
+sns.countplot(iris['species'])
+plt.show
+print(iris['species'].unique())
+print(iris.shape)
