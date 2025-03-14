6652a5eb661c5c844d4b1cf4b14cae8e63b77759
Harisudhan4127 <harisudhan4127@gmail.com>
2025-03-15 00:49:28 +0530

    day6

--- day6/mini_project.py
+++ day6/mini_project.py
@@ -19,28 +19,48 @@ corr = df.corr()
 test = df.groupby('sex').agg({'target' : 'count'})
 # print(test)
 
+# Total count of target
 # print(df['target'].value_counts())
 
-cp_gp = df.groupby('cp').agg({'target' : 'count'})
-print(cp_gp)
-print(df['cp'].value_counts())
+# cp_gp = df.groupby('cp').agg({'target' : 'count'})
+# print(cp_gp)
+# print(df['cp'].value_counts())
 
 # correlation bt sex and target
 # sns.barplot(x = df['sex'], y = df['target'])
-
-# outlet checking 
-
-# sns.boxplot(df['trestbps'])
-# sns.boxplot(df['chol'])
-# sns.boxplot(df['trestbps'])
-
 # Correlation cp and target
-sns.barplot(x = df['cp'], y = df['target'])
+# sns.barplot(x = df['cp'], y = df['target'])
 
 # sns.pairplot(df, hue = 'sex')
-
 # sns.countplot(df['target'])
+
+# outlet checking
+ 
+# chol leveling checking
+# max persons in chol range
+# sns.swarmplot(df['chol'])
+
+# Surgar level checking
+# print(df['fbs'].unique())
+# print(df['fbs'].value_counts())
+
+# ECG checking
+# print(df['restecg'].unique())
+# print(df['restecg'].value_counts())
+# sns.swarmplot(df['restecg'])
+# sns.barplot(x= df['fbs'], y= df['target'])
+
+# Trest Bps checking
+# sns.boxplot(df['trestbps'])
+# sns.boxplot(df['trestbps'])
+
+
+# thalach
+thalach = df['thalach'] > 160
+print(thalach.value_counts())
+
 plt.show()
+
 # print(x)
 # print(y)
 
