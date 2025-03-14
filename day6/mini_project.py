efe4faa7bf578265f3bd3608608ea3b7f228521c
Harisudhan4127 <harisudhan4127@gmail.com>
2025-03-15 02:09:49 +0530

    day 6

--- day6/mini_project.py
+++ day6/mini_project.py
@@ -1,12 +1,13 @@
+from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 from sklearn.model_selection import  train_test_split
 import seaborn as sns
+from sklearn.neighbors import KNeighborsClassifier 
 import pandas as pd 
 
 df = pd.read_csv('Data/heart.csv')
 # print(df.head())
-x = df.iloc[:,:13]
-y = df['target']
+target = df['target']
 
 corr = df.corr()
 # sns.heatmap(corr)
@@ -55,10 +56,22 @@ test = df.groupby('sex').agg({'target' : 'count'})
 # sns.boxplot(df['trestbps'])
 
 
-# thalach
+# thalach checking
 thalach = df['thalach'] > 160
-print(thalach.value_counts())
+# print(thalach.value_counts())
 
+# exang checking 
+# exang = df['exang']
+# print(exang.unique())
+
+# oldpeak 
+# oldpeak = df['oldpeak']
+# sns.boxplot(oldpeak)
+
+# slope
+slope = df['slope']
+# print(slope.unique())
+# sns.barplot(x = slope, y = target)
 plt.show()
 
 # print(x)
@@ -68,3 +81,14 @@ plt.show()
 # print(df.isna().sum())
 # print(df['target'].unique())
 
+x = df.iloc[:,:13]
+y = df['target']
+
+x_train, x_test, y_train , y_test = train_test_split(x,y, test_size= 0.2, random_state= 50)
+
+knn = KNeighborsClassifier(n_neighbors= 4)
+knn.fit(x_train, y_train)
+
+prediction = knn.predict(x_test)
+
+print(accuracy_score(prediction, y_test)*100)
