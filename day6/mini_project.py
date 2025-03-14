c844d74498bbae6fc6696f41dd17646834b029e0
Harisudhan4127 <harisudhan4127@gmail.com>
2025-03-15 04:25:02 +0530

    day6

--- day6/mini_project.py
+++ day6/mini_project.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import  train_test_split
 from sklearn.neighbors import KNeighborsClassifier 
 import pandas as pd 
-
+from sklearn.ensemble import RandomForestClassifier
 df = pd.read_csv('Data/heart.csv')
 target = df['target']
 
@@ -11,11 +11,15 @@ target = df['target']
 x = df.iloc[:,:13]
 y = df['target']
 
-x_train, x_test, y_train , y_test = train_test_split(x,y, test_size= 0.2, random_state= 50)
+x_train, x_test, y_train , y_test = train_test_split(x,y, test_size= 0.2, random_state= 42)
 
 knn = KNeighborsClassifier(n_neighbors= 4)
 knn.fit(x_train, y_train)
 
+rf = RandomForestClassifier(random_state= 300)
+rf.fit(x_train, y_train)
+predict = rf.predict(x_test)
 prediction = knn.predict(x_test)
 
-print(accuracy_score(prediction, y_test)*100)
+# print(accuracy_score(prediction, y_test)*100)
+print(accuracy_score(predict, y_test)*100)
